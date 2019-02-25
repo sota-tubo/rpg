5a99a3fd81c13d93330a6c28ef73e3f3e0f1d2f0
sota <sota.tubo@gmail.com>
2019-02-26 05:24:11 +0900

    全職業実装
    現時点で実装されている全職業を実装
    (アプリ側にはまだ実装されていません)

--- Assets/Scripts/HighMagic.cs
+++ Assets/Scripts/HighMagic.cs
@@ -6,12 +6,20 @@ public class HighMagic : MonoBehaviour {
 
 	[SerializeField]
 	private GameObject highmagic = null;
+	[SerializeField]
+	private GameObject Heal, HiHeal; //闇の戦士用
 
 	// Use this for initialization
 	void Start () {
-		if (selectJob.Sag == true)
+		if (selectJob.Sag == true || selectJob.Dar == true || selectJob.Yuu == true)
 		{
 			highmagic.SetActive(true);
+		}
+
+		if (selectJob.Dar == true)
+		{
+			Heal.SetActive(false);
+			HiHeal.SetActive(false);
 		}
 	}
 	
